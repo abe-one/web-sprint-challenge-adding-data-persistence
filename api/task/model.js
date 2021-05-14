e500b51c6f3d6b6c7d69b1380ca4a0594df1b940
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-14 15:07:53 -0500

    joint task, project query

--- api/task/model.js
+++ api/task/model.js
@@ -1,11 +1,25 @@
 const db = require("../../data/dbConfig");
 
-exports.getAllTasks = () => db("tasks");
+const toBool = (task) => {
+  task.task_completed = !!task.task_completed;
+  return task;
+};
+
+exports.getAllTasks = async () => {
+  function onFunc() {
+    this.on("t.project_id", "p.project_id");
+  }
+
+  const tasks = await db
+    .select("t.*", "p.project_name", "p.project_description")
+    .from("tasks as t")
+    .join("projects as p", onFunc);
+  return tasks.map(toBool);
+};
 
 exports.getTaskById = async (id) => {
   const task = await db("tasks").where("task_id", id).first();
-  task.task_completed = !!task.task_completed;
-  return task;
+  return toBool(task);
 };
 
 exports.insertTask = async (task) => {
